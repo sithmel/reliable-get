73b4340310da7e10d6c1cd7371d89a72c45eba0e
cliftonc <clifton.cunningham@gmail.com>
2014-11-18 13:27:15 +0000

    Update test to trigger CI

--- tests/reliable-get.test.js
+++ tests/reliable-get.test.js
@@ -8,10 +8,10 @@ describe("Core caching", function() {
   it('should request something', function(done) {
 
       var config = {cache:{engine:'memory'}};
-      var rc = ReliableGet(config);
-      rc({url:'http://www.google.com'}, function(err, response) {
+      var rg = ReliableGet(config);
+      rg({url:'http://www.google.com'}, function(err, response) {
           done();
-      })
+      });
 
   });
 
